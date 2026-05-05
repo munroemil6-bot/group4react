791225e39cdd3a9218d298e39220cf097e7481dd
Jane Akinyi Nyasoro <janenyasoro@gmail.com>
2026-05-05 12:13:58 +0300

    Adding the Login page using firebase

---
+++ voting-poll-app/src/components/Login.jsx
@@ -0,0 +1,23 @@
+// src/components/Login.jsx
+import { signInWithPopup } from "firebase/auth";
+import { auth, googleProvider } from "../firebase"; // Import your configured auth and provider
+
+function Login() {
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+      // User is signed in successfully
+      console.log("User signed in!");
+    } catch (error) {
+      console.error("Sign-in error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    </div>
+  );
+}
+
+export default Login;
