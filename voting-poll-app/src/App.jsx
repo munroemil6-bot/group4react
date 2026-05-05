7a3f2e20f64a325e0bc788a52bc5a2783b8142a3
Ck-kosh <elias.cheruiyot@student.moringaschool.com>
2026-05-05 12:31:32 +0300

    modify

--- voting-poll-app/src/App.jsx
+++ voting-poll-app/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState, useEffect } from "react";
 import PollForm from "./components/PollForm";
 import PollList from "./components/PollList";
-<<<<<<< HEAD
-// import Login from "./components/Login";
+import Login from "./components/Login";
 /**import LogoutButton from "./components/LogoutButton";**/
 import { useAuth } from "./hooks/useAuth";
 import './App.css'
-=======
->>>>>>> 0548312b65da5fe534260895e6ade3f073152403
 
 function App() {
   const [options, setOptions] = useState([]);
